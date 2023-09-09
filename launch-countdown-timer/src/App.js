d89e03f0dd0a4958ba5a14f6851fa10aa230f4c2
Maxtreme69 <s3853476@student.rmit.edu.au>
2023-09-10 00:21:42 +1000

    add pattern hills image
    added pattern-hills.svg image to stick to bottom of screen and stretch full width of screen.

--- launch-countdown-timer/src/App.js
+++ launch-countdown-timer/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import PatternHills from './img/pattern-hills.svg';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="pattern-hills">
+        <img src={PatternHills} alt="Pattern Hills"/>
+      </div>
     </div>
   );
 }
