b0b15aeb967c03bb85f7d4438990944a6639ff11
Maxtreme69 <s3853476@student.rmit.edu.au>
2023-09-10 01:00:26 +1000

    add icons
    added social media icons to bottom of page

--- launch-countdown-timer/src/App.js
+++ launch-countdown-timer/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import PatternHills from './img/pattern-hills.svg';
+import FaceBookIcon from './img/icon-facebook.svg'; 
+import PinterestIcon from './img/icon-pinterest.svg';
+import InstagramIcon from './img/icon-instagram.svg';
 
 function App() {
   return (
     <div className="App">
       <div className="pattern-hills">
-        <img src={PatternHills} alt="Pattern Hills"/>
+        <img src={PatternHills} alt="Pattern Hills" />
+      </div>
+      <div className="icon-container">
+        <img src={FaceBookIcon} alt="Facebook Icon" />
+        <img src={PinterestIcon} alt="Pinterest Icon" />
+        <img src={InstagramIcon} alt="Instagram Icon" />
       </div>
     </div>
   );
